ff68d235e1725ec6a8f287ece6c5422a1ffd7a9e
MicroHub69 <msodq2018@gmail.com>
2022-03-14 11:23:48 +0100

    DAY 04

--- blogs/README.md
+++ blogs/README.md
@@ -1,3 +1,4 @@
 **50 Days with React JS**
 
 This Respository focus majorly on 50 Days Pratical Challenge with ReactJS.
+
---
+++ movieapp/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { useState } from "react";
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
+    </div>
+  );
+}
